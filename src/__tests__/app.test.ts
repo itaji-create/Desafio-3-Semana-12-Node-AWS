9ac4eba349093ec05b227ec2442895ac298db161
itaji-create <itaji.1999@gmail.com>
2024-07-31 13:00:59 -0300

    chore: inicia projeto configurando as dependências necessárias

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest';
+import app from '../app';
+
+describe('GET /', () => {
+  it('Deve retornar "Hello, World" com status code 200, World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello, World!');
+  });
+});
