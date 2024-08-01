3879e52d581bf34f55e854f8260023c13eae8064
itaji-create <itaji.1999@gmail.com>
2024-08-01 15:29:15 -0300

    refactor: remove a conexão que estava no app

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,23 +1,15 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
-import AppError from '@shared/errors/AppError';
+//import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import mongoose from 'mongoose';
 import 'dotenv/config';
+import connectDB from '@config/database';
 
 const app = express();
+connectDB();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/data', {})
-  .then(() => {
-    console.log('Conectado ao MongoDB');
-  })
-  .catch((err) => {
-    console.error('Erro ao conectar ao MongoDB', err);
-  });
 
 app.use('/v1', routes);
 
