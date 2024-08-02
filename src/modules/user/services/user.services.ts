6698d1393e60ee265777f3b2bd298da4beaaa293
itaji-create <itaji.1999@gmail.com>
2024-08-02 17:53:02 -0300

    feat: adiciona encriptação do md5 para salvar senhas dos usuários

--- src/modules/user/services/user.services.ts
+++ src/modules/user/services/user.services.ts
@@ -1,13 +1,14 @@
 import { IUser } from '@shared/interfaces/user.interface';
 import User from '../models/user.model';
 import AppError from '@shared/errors/AppError';
+import { Md5 } from 'ts-md5';
 
 export const createUser = async (userData: IUser) => {
   const existingUser = await User.findOne({ email: userData.email });
   if (existingUser) {
     throw new AppError('Email already in use', 400, 'Bad Request');
   }
-
+  userData.password = Md5.hashStr(userData.password);
   const user = new User(userData);
   return await user.save();
 };
