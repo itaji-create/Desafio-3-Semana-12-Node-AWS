93d5c3b14b0d0d8e3f4f19455f96716b16092fdb
itaji-create <itaji.1999@gmail.com>
2024-08-02 07:28:05 -0300

    feat: adiciona logica para instanciar o db em modo de prod, dev e test

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,25 @@
 import mongoose from 'mongoose';
 
+type Environment = 'prod' | 'production' | 'dev' | 'development' | 'test';
+
+const environment: Environment = (
+  process.env.NODE_ENV || 'test'
+).toLowerCase() as Environment;
+
+const suffix: Record<Environment, string> = {
+  prod: '',
+  production: '',
+  dev: '-dev',
+  development: '-dev',
+  test: '-test',
+};
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/data',
-      {},
-    );
+    const uri = process.env.MONGODB_URI
+      ? `${process.env.MONGODB_URI}${suffix[environment]}`
+      : `mongodb://127.0.0.1:27017/database_test`;
+    await mongoose.connect(uri, {});
     console.log('Conectado ao MongoDB');
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB', error);
