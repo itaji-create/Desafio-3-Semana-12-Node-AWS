516c64cb374f25a55f1fa839adbd17874486d22e
itaji-create <itaji.1999@gmail.com>
2024-08-03 12:00:37 -0300

    refactor: adiciona constantes para as urls de sign-in e sign-up e corrige status code de errors

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -3,6 +3,9 @@ import app from '../shared/http/app';
 import { connectDB, disconnectDB } from '@config/database';
 import 'dotenv/config';
 
+const urlSignUp = '/users/sign-up';
+const urlSignIn = '/users/sign-in';
+
 describe('Testes da rota de cadastro dos usuários', () => {
   beforeAll(async () => {
     await connectDB();
@@ -24,26 +27,26 @@ describe('Testes da rota de cadastro dos usuários', () => {
   };
 
   test('Cria um novo usuário', async () => {
-    const res = await request(app).post('/v1/users/sign-up').send(userData);
+    const res = await request(app).post(urlSignUp).send(userData);
     expect(res.status).toBe(201);
     expect(res.body.firstName).toBe('string');
   });
 
-  test('Falha ao tentar criar usuário com email já existente no db', async () => {
-    const res2 = await request(app).post('/v1/users/sign-up').send(userData);
+  test('Falha ao tentar criar usuário com email já existente no database', async () => {
+    const res2 = await request(app).post(urlSignUp).send(userData);
     expect(res2.status).toBe(400);
   });
 
   test('Cria usuário com um novo email', async () => {
     userData.email = 'email.teste@gmail.com';
-    const res2 = await request(app).post('/v1/users/sign-up').send(userData);
+    const res2 = await request(app).post(urlSignUp).send(userData);
     expect(res2.status).toBe(201);
   });
 
   test('Falha ao tentar criar usuário com senha divergente da confimação de senha', async () => {
     userData.email = 'email.teste3@gmail.com';
     userData.confirmPassword = 'anotherPassword';
-    const res2 = await request(app).post('/v1/users/sign-up').send(userData);
+    const res2 = await request(app).post(urlSignUp).send(userData);
     expect(res2.status).toBe(400);
   });
 });
@@ -51,13 +54,13 @@ describe('Testes da rota de cadastro dos usuários', () => {
 describe('Testes da rota de login dos usuários', () => {
   beforeAll(async () => {
     await connectDB();
-    await request(app).post('/v1/users/sign-up').send(userData);
+
+    await request(app).post(urlSignUp).send(userData);
   });
 
   afterAll(async () => {
     await disconnectDB();
   });
-
   const userData = {
     firstName: 'string',
     lastName: 'string',
@@ -69,31 +72,33 @@ describe('Testes da rota de login dos usuários', () => {
     confirmPassword: 'string',
   };
 
-  test('Faz login em um novo usuário', async () => {
-    const user = {
-      email: userData.email,
-      password: userData.password,
-    };
-    const res = await request(app).post('/v1/users/sign-in').send(user);
+  const login = {
+    email: userData.email,
+    password: userData.password,
+  };
+
+  test('Faz login com os dados corretos e retorna um token no headers', async () => {
+    const res = await request(app).post(urlSignIn).send(login);
+
     expect(res.status).toBe(200);
-    expect(res.body.firstName).toBe('string');
+    expect(res.headers.authorization).toMatch(
+      /^Bearer\s[A-Za-z0-9\-._~+/]+=*$/,
+    );
   });
 
   test('Falha ao tentar login com senha errada', async () => {
-    const user = {
-      email: userData.email,
-      password: 'wrongPassword',
-    };
-    const res = await request(app).post('/v1/users/sign-in').send(user);
-    expect(res.status).toBe(400);
+    login.password = 'wrongPassword';
+    login.email = userData.email;
+
+    const res = await request(app).post(urlSignIn).send(login);
+    expect(res.status).toBe(404);
   });
 
   test('Falha ao tentar login com email inexistente', async () => {
-    const user = {
-      email: 'wrongEmail@gmail.com',
-      password: 'string',
-    };
-    const res = await request(app).post('/v1/users/sign-in').send(user);
-    expect(res.status).toBe(400);
+    login.email = 'wrongEmail@email.com';
+    login.password = userData.password;
+
+    const res = await request(app).post(urlSignIn).send(login);
+    expect(res.status).toBe(404);
   });
 });
